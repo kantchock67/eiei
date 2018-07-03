2a75ff2d39a810cf6e6ca10616a722be5fc2e70d
Nara <emachines1646@gmail.com>
2018-07-03 10:57:14 +0700

    Initial Commit

---
+++ eiei pj/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  eiei pj
+//
+//  Created by NaRaWiTh on 3/7/2561 BE.
+//  Copyright © 2561 nara dev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
